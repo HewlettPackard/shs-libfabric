ba69d64f7be22f9ad97d901eed8e2675c4573807
Ian Ziemba <ian.ziemba@hpe.com>
2022-03-21 16:37:07 -0500

    prov/cxi: Refactor rdzv PltTE alloc
    Minor refactor of rendezvous PtlTE alloc by creating helper functions.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
    
    prov/cxi: Remove unused shared context code
    
    This functionality is not supported and supporting dead code is a waste
    of time. Thus, remove it.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
    
    prov/cxi: Refactor rendezvous PtlTE
    
    Create a rendezvous PtlTE object and simplify the allocation and freeing
    of the corresponding LEs.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>

--- prov/cxi/src/cxip_txc.c
+++ prov/cxi/src/cxip_txc.c
@@ -18,19 +18,6 @@
 #define CXIP_WARN(...) _CXIP_WARN(FI_LOG_EP_CTRL, __VA_ARGS__)
 
 /*
- * cxip_rdzv_pte_cb() - Process rendezvous source PTE state change events.
- */
-void cxip_rdzv_pte_cb(struct cxip_pte *pte, const union c_event *event)
-{
-	switch (pte->state) {
-	case C_PTLTE_ENABLED:
-		break;
-	default:
-		CXIP_FATAL("Unexpected state received: %u\n", pte->state);
-	}
-}
-
-/*
  * txc_msg_init() - Initialize an RX context for messaging.
  *
  * Allocates and initializes hardware resources used for transmitting messages.
@@ -40,9 +27,6 @@ void cxip_rdzv_pte_cb(struct cxip_pte *pte, const union c_event *event)
 static int txc_msg_init(struct cxip_txc *txc)
 {
 	int ret;
-	struct cxi_pt_alloc_opts pt_opts = {
-		.is_matching = 1,
-	};
 
 	/* Allocate TGQ for posting source data */
 	ret = cxip_ep_cmdq(txc->ep_obj, txc->tx_id, false, FI_TC_UNSPEC,
@@ -52,42 +36,18 @@ static int txc_msg_init(struct cxip_txc *txc)
 		return -FI_EDOMAIN;
 	}
 
-	if (txc->ep_obj->av->attr.flags & FI_SYMMETRIC) {
-		CXIP_DBG("Using logical PTE matching\n");
-		pt_opts.use_logical = 1;
-	}
-
-	/* Reserve the Rendezvous Send PTE */
-	ret = cxip_pte_alloc(txc->ep_obj->if_dom[txc->tx_id],
-			     txc->send_cq->eq.eq,
-			     txc->domain->iface->dev->info.rdzv_get_idx,
-			     false, &pt_opts, cxip_rdzv_pte_cb, txc,
-			     &txc->rdzv_pte);
-	if (ret != FI_SUCCESS) {
-		CXIP_WARN("Failed to allocate RDZV PTE: %d\n", ret);
-		goto put_rx_cmdq;
-	}
-
-	ret = cxip_pte_set_state_wait(txc->rdzv_pte, txc->rx_cmdq, txc->send_cq,
-				      C_PTLTE_ENABLED, 0);
-	if (ret != FI_SUCCESS) {
-		CXIP_WARN("Failed to enqueue command: %d\n", ret);
-		goto free_rdzv_pte;
-	}
-
-	ret = cxip_txc_zbp_init(txc);
+	ret = cxip_rdzv_pte_alloc(txc, &txc->rdzv_pte);
 	if (ret) {
-		CXIP_WARN("Failed to initialize ZBP: %d\n", ret);
-		goto free_rdzv_pte;
+		CXIP_WARN("Failed to allocate rendezvous PtlTE: %d:%s\n", ret,
+			  fi_strerror(-ret));
+		goto err_put_rx_cmdq;
 	}
 
 	CXIP_DBG("TXC RDZV PtlTE enabled: %p\n", txc);
 
 	return FI_SUCCESS;
 
-free_rdzv_pte:
-	cxip_pte_free(txc->rdzv_pte);
-put_rx_cmdq:
+err_put_rx_cmdq:
 	cxip_ep_cmdq_put(txc->ep_obj, txc->tx_id, false);
 
 	return ret;
@@ -103,9 +63,7 @@ put_rx_cmdq:
  */
 static int txc_msg_fini(struct cxip_txc *txc)
 {
-	cxip_txc_zbp_fini(txc);
-	cxip_txc_rdzv_src_fini(txc);
-	cxip_pte_free(txc->rdzv_pte);
+	cxip_rdzv_pte_free(txc->rdzv_pte);
 	cxip_ep_cmdq_put(txc->ep_obj, txc->tx_id, false);
 
 	return FI_SUCCESS;
@@ -247,8 +205,7 @@ static void txc_disable(struct cxip_txc *txc)
  *
  * Used to support creating a TX context for fi_endpoint() or fi_tx_context().
  */
-static struct cxip_txc *txc_alloc(const struct fi_tx_attr *attr, void *context,
-				  int use_shared, size_t fclass)
+struct cxip_txc *cxip_txc_alloc(const struct fi_tx_attr *attr, void *context)
 {
 	struct cxip_txc *txc;
 
@@ -258,58 +215,19 @@ static struct cxip_txc *txc_alloc(const struct fi_tx_attr *attr, void *context,
 
 	dlist_init(&txc->ep_list);
 	fastlock_init(&txc->lock);
-	fastlock_init(&txc->rdzv_src_lock);
 	ofi_atomic_initialize32(&txc->otx_reqs, 0);
-	ofi_atomic_initialize32(&txc->zbp_le_linked, 0);
-	ofi_atomic_initialize32(&txc->rdzv_src_lacs, 0);
-	dlist_init(&txc->rdzv_src_reqs);
 	dlist_init(&txc->msg_queue);
 	dlist_init(&txc->fc_peers);
 
-	switch (fclass) {
-	case FI_CLASS_TX_CTX:
-		txc->fid.ctx.fid.fclass = FI_CLASS_TX_CTX;
-		txc->fid.ctx.fid.context = context;
-		txc->fclass = FI_CLASS_TX_CTX;
-		txc->use_shared = use_shared;
-		break;
-	case FI_CLASS_STX_CTX:
-		txc->fid.stx.fid.fclass = FI_CLASS_STX_CTX;
-		txc->fid.stx.fid.context = context;
-		txc->fclass = FI_CLASS_STX_CTX;
-		break;
-	default:
-		goto err;
-	}
-
+	txc->fid.ctx.fid.fclass = FI_CLASS_TX_CTX;
+	txc->fid.ctx.fid.context = context;
+	txc->fclass = FI_CLASS_TX_CTX;
 	txc->attr = *attr;
 	txc->max_eager_size = cxip_env.rdzv_threshold + cxip_env.rdzv_get_min;
 	txc->rdzv_eager_size = cxip_env.rdzv_eager_size;
 	txc->hmem = !!(attr->caps & FI_HMEM);
 
 	return txc;
-
-err:
-	fastlock_destroy(&txc->lock);
-	free(txc);
-	return NULL;
-}
-
-/*
- * cxip_stx_alloc() - Allocate a regular (not shared) TX context.
- */
-struct cxip_txc *cxip_txc_alloc(const struct fi_tx_attr *attr, void *context,
-				int use_shared)
-{
-	return txc_alloc(attr, context, use_shared, FI_CLASS_TX_CTX);
-}
-
-/*
- * cxip_stx_alloc() - Allocate a shared TX context.
- */
-struct cxip_txc *cxip_stx_alloc(const struct fi_tx_attr *attr, void *context)
-{
-	return txc_alloc(attr, context, 0, FI_CLASS_STX_CTX);
 }
 
 /*
