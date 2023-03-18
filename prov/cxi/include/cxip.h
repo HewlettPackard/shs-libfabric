382b0b9240a0d4ecbae26221cbd1859daade016c
Steve Welch <welch@hpe.com>
2023-03-18 17:24:34 -0500

    prov/cxi: Ensure EP progress progresses entire EP
    Calling a progress function should progress the entire
    endpoint. While with MPI this is already done since it
    uses a single CQ, there is not a guarantee of this in
    general. This will allow for software rendezvous get
    EVENT_REPLYs to be reported to the TX event queue and
    utilize the TX credit scheme.
    
    NETCASSINI-4973
    
    Signed-off-by: Steve Welch <welch@hpe.com>
    
    prov/cxi: Ack unacked events when done with evtq processing
    
    Ack any unacked events before exiting event queue progress.
    Leaving partial batched acks can lead to overflowing the EQ
    when slots reserved == slots free and a flow control is
    entered, i.e. Flush of appends could cause overflow.
    
    NETCASSINI-4973
    
    Signed-off-by: Steve Welch <welch@hpe.com>
    
    prov/cxi: Report S/W rendezvous get EVENT_REPLY to TX eventq
    
    Initiator TX events interfere with the receive EQ slot management
    and can cause overflows when the EVENT_REPLY is received. Direct
    software initiated get events to the TX event queue and adjust
    the TX credit scheme to look at both TX initiated and RX Rget
    initiated credits to avoid overflow of TX event queue.
    
    NETCASSINI-4973
    
    Signed-off-by: Steve Welch <welch@hpe.com>

--- prov/cxi/include/cxip.h
+++ prov/cxi/include/cxip.h
@@ -233,6 +233,7 @@ struct cxip_environment {
 	size_t req_buf_size;
 	size_t req_buf_min_posted;
 	size_t req_buf_max_cached;
+	int sw_rx_tx_init_max;
 	int msg_lossless;
 	size_t default_cq_size;
 	size_t default_tx_size;
@@ -1592,6 +1593,15 @@ cxip_msg_counters_msg_record(struct cxip_msg_counters *cntrs,
 }
 
 /*
+ * The default for the number of SW initiated TX operation that may
+ * be initiated by RX processing and be outstanding. This has no
+ * impact on hardware initiated rendezvous gets. This value can be
+ * adjusted if necessary with FI_CXI_SW_RX_TX_INIT_MAX=#.
+ */
+#define CXIP_SW_RX_TX_INIT_MAX_DEFAULT	1024
+#define CXIP_SW_RX_TX_INIT_MIN		64
+
+/*
  * Endpoint object receive context
  */
 struct cxip_rxc {
@@ -1615,6 +1625,8 @@ struct cxip_rxc {
 	struct cxip_cmdq *tx_cmdq;	// TX CMDQ for Message Gets
 
 	ofi_atomic32_t orx_reqs;	// outstanding receive requests
+	ofi_atomic32_t orx_tx_reqs;	// outstanding RX initiated TX requests
+	int32_t max_tx;
 	unsigned int recv_appends;
 
 	/* Window when FI_CLAIM mutual exclusive access is required */
@@ -2602,10 +2614,7 @@ struct cxip_req *cxip_evtq_req_alloc(struct cxip_evtq *evtq,
 void cxip_evtq_req_free(struct cxip_req *req);
 void cxip_evtq_progress(struct cxip_evtq *evtq);
 
-void cxip_ep_progress(struct fid *fid, struct cxip_cq *cq);
-/* Optimizations of the above that can be used later */
-void cxip_ep_tx_progress(struct cxip_ep_obj *ep_obj);
-void cxip_ep_rx_progress(struct cxip_ep_obj *ep_obj);
+void cxip_ep_progress(struct fid *fid);
 int cxip_ep_peek(struct fid *fid);
 void cxip_ep_flush_trig_reqs(struct cxip_ep_obj *ep_obj);
 
