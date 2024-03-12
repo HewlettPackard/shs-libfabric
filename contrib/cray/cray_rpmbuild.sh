74442ea9e5dfd9cca46a71c16aac2f9bb406c13e
Rakesh Kumar Boyanapalli <rakesh-kumar.boyanapalli@hpe.com>
2024-03-12 13:15:28 +0530

    Task: Update cray_rpmbuild to utilize the hpc-shs-version repo

--- contrib/cray/cray_rpmbuild.sh
+++ contrib/cray/cray_rpmbuild.sh
@@ -122,22 +122,29 @@ if [[ -z "$BUILD_METADATA" ]] ; then
 		RELEASE=1
 	fi
 else
-	BRANCH=`git branch --show-current`
-	git clone https://$HPE_GITHUB_TOKEN@github.hpe.com/hpe/hpc-sshot-slingshot-version.git
-
-	cd hpc-sshot-slingshot-version
-
-	if ! git checkout $BRANCH_NAME ; then
-	echo "INFO: Branch '$BRANCH_NAME' does not exist in hpc-sshot-slingshot-version repo, using version string from master branch"
+	BRANCH=`git branch --show-current || git rev-parse --abbrev-ref HEAD`
+	
+	if [ -d hpc-shs-version ]; then
+    		git -C hpc-shs-version pull
 	else
-	echo "INFO: Using Slingshot release version from '$BRANCH_NAME'"
+    		if [[ -n "${SHS_LOCAL_BUILD}" ]]; then
+        		git clone git@github.hpe.com:hpe/hpc-shs-version.git
+    		else
+    			git clone https://$HPE_GITHUB_TOKEN@github.hpe.com/hpe/hpc-shs-version.git
+    		fi
 	fi
 
-	cd -
+	. hpc-shs-version/scripts/get-shs-version.sh
+	. hpc-shs-version/scripts/get-shs-label.sh
 
-	PRODUCT_VERSION=$(cat hpc-sshot-slingshot-version/slingshot-version|| echo "0.0.0")
-	echo "INFO: Slingshot release version '$PRODUCT_VERSION'"
-	RELEASE="SSHOT${PRODUCT_VERSION}_${BUILD_METADATA}"
+	PRODUCT_VERSION=$(get_shs_version)
+	PRODUCT_LABEL=$(get_shs_label)
+	
+	echo "INFO: Using SHS release version from BRANCH: '$BRANCH_NAME'"
+	echo
+	echo "INFO: SHS release version '$PRODUCT_VERSION'"
+	
+	RELEASE="${PRODUCT_LABEL}${PRODUCT_VERSION}_${BUILD_METADATA}"
 fi
 
 
